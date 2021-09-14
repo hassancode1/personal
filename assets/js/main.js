5e9380491cec54292d1aa15cdb798d01abb74337
hassancode1 <hassanjaeh@gmail.com>
2021-09-13 22:49:53 -0700

    h

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,46 +25,37 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== SCROLL REVEAL ANIMATION =====
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-});
+const modalViews = document.querySelectorAll(".services__modal");
+modalBtns = document.querySelectorAll(".services__button");
+modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function(modalClicks){
+ modalViews[modalClicks].classList.add("active-modal")
+}
+modalBtns.forEach((modalBtn, i)=>{
+
+  modalBtn.addEventListener("click", ()=>{
+    modal(i)
+  })
+})
+
+modalCloses.forEach((modalCLose) =>{
+  modalCLose.addEventListener("click", () =>{
+    modalViews.forEach((modalView)=>{
+      modalView.classList.remove("active-modal")
+    })
+  })
+})
 
 
 
 
-/*SCROLL HOME
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-
-/*SCROLL ABOUT
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
-
-/*SCROLL SKILLS
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
-
-/*SCROLL WORK
-sr.reveal('.work__img',{interval: 200}); */
-
-/*SCROLL CONTACT
-sr.reveal('.contact__input',{interval: 200}); 
-*/
-
-
-var swiper = new Swiper(".swiper", {
+var swiperP = new Swiper(".swiper", {
   
    loop:true,
     cssMode: true,
+    
+   grabCursor:true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -76,3 +67,22 @@ var swiper = new Swiper(".swiper", {
   
   });
   
+  
+var swiperT = new Swiper(".swiper-container", {
+  
+  loop:true,
+     spaceBetween:48,
+     grabCursor:true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable:true,
+     dynamicBullets:true
+   },
+   breakpoints:{
+   768:{
+     slidesPerView:2,
+   }
+   }
+ 
+ });
+ 
