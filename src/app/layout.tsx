5f72179091d953fa754274e090c83f519f3d93d2
Hassan Iliyasu <hassanjaeh@gmail.com>
2025-12-18 14:16:12 +0100

    fixed images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
               <OnekoCat />
               <Quote />
               <Footer />
-              <ChatBubble />
+              {/* <ChatBubble /> */}
               {/* <UmamiAnalytics /> */}
             </ReactLenis>
           </ThemeProvider>
